04831cf309736120d2854167e08930dd71332add
jacksoncougar <jacksoncougar@gmail.com>
2015-05-19 23:24:05 -0600

    Save Cache
    added extension method to save cache

--- main.cs
+++ main.cs
@@ -26,32 +26,27 @@ namespace Moonfish
         [STAThread]
         private static void Main()
         {
-            using (var map = new Moonfish.Cache.CacheStream(Path.Combine(Local.MapsDirectory, @"output.map")))
-            using (var output = new FileStream(Path.Combine(Local.MapsDirectory, @"output1.map"), FileMode.Create,
-                    FileAccess.Write, FileShare.ReadWrite, 4 * 1024, FileOptions.SequentialScan))
-            {
-                map.SaveTo(output);
-            } 
-            return;
-            {
-                var test = new Moonfish.Cache.CacheStream(Path.Combine(Local.MapsDirectory, @"output.map"));
-                test.Seek(test.Index.GlobalsIdent);
-                var position = test.GetFilePosition();
-
-                var buggery = test.GetOwner(187923790);
-                //test.Deserialize(buggery.Identifier);
-
-                var localOffset = 187923790 - test.VirtualAddressToFileOffset(buggery.VirtualAddress);
-
-                foreach (var data in test.Index)
-                {
-                    test.Deserialize(data.Identifier);
-                }
-            }
+            CacheStream map = new CacheStream(Path.Combine(Local.MapsDirectory, @"output1.map"));
+            map.Save();
 
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Gizmo());
+        }
+
+        private static void Save(this CacheStream map)
+        {
+            var filename = Path.Combine(Local.MapsDirectory, @"temp.map");
+            FileStream copyStream = new FileStream(filename, FileMode.Create,
+                FileAccess.Write, FileShare.ReadWrite, 4 * 1024, FileOptions.SequentialScan);
+            using(copyStream)
+            using (map)
+            {
+                map.SaveTo(copyStream);
+            }
+            File.Delete(map.Name);
+            File.Move(filename, map.Name);
+            map = new CacheStream(map.Name);
         }
     }
 }
